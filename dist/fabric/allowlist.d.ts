3afbf0035700e6a5a827501c93839453484a0b0a
Zac Ruiz <zac@salt.io>
2025-12-28 12:17:24 -0500

    Add .gitignore and remove build artifacts from tracking

--- dist/fabric/allowlist.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const CompanyMetricAllowlist: Set<string>;
-export declare const FilingMetricAllowlist: Set<string>;
-export declare const BondMetricAllowlist: Set<string>;
--- dist/sec/client.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.fetchSubmissions = void 0;
-const http_1 = require("../util/http");
-const fetchSubmissions = async (cik, clientOptions) => {
-    const url = `https://data.sec.gov/submissions/CIK${cik}.json`;
-    return (0, http_1.fetchJson)(url, clientOptions);
-};
-exports.fetchSubmissions = fetchSubmissions;
